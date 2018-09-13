f65d543afed5582511ad93bcd7fe7daa047aa60e
Juanjo <jherrero@traity.com>
2018-09-13 15:15:40 +0200

    Fix Contract related issues

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import App from "./app";
+import Contract from "./contracts";
 import * as SignStrategies from "./sign-strategies";
 import * as Utils from "./utils";
 
@@ -6,6 +7,7 @@ export * from "./structs";
 export * from "./types";
 export {
   App,
+  Contract,
   SignStrategies,
   Utils,
 }
