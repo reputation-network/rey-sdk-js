64e5d0d2b242e81052aa4a606b722a5166c32938
Juan Jose Herrero <jherrero@traity.com>
2018-09-21 12:00:44 +0000

    Fix linting error

--- src/types.d/web3-eth-accounts/index.d.ts
+++ src/types.d/web3-eth-accounts/index.d.ts
@@ -1,5 +1,5 @@
 declare module "web3-eth-accounts" {
-  type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>
+  type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
   export interface Signature {
     message: string;
     messageHash: string;
