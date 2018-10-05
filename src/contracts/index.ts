12d9c8480c83d9a796e36e7d6c804ef22745d787
Juanjo <jherrero@traity.com>
2018-10-05 12:20:45 +0200

    Add testnet contract shorthand

--- src/contracts/index.ts
+++ src/contracts/index.ts
@@ -7,3 +7,10 @@ export function DevelopmentContract() {
     "0x556ED3bEaF6b3dDCb1562d3F30f79bF86fFC05B9",
   );
 }
+
+export function TestnetContract(nodeUrl: string) {
+  return new RegistryContract(
+    nodeUrl,
+    "0xC05f9be01592902e133F398998E783b6cbD93813",
+  );
+}
