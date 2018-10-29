609516e3acfcdb8d89ee1299285ebb94bce93b79
Juanjo <jherrero@traity.com>
2018-10-29 11:06:21 +0100

    Cleanup dependencies

--- src/types.d/crypto/index.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "crypto" {
-  export function generateKeyPair(format: string, opts: any, callback: Function): Promise<Key>;
-
-  export type Key = {
-    publicKey: string;
-    privateKey?: string;
-  }
-}
