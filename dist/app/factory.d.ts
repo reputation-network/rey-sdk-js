7ec5e14b76b80fa612edfa576c41971e71aba7b5
Juan Jose Herrero <jherrero@traity.com>
2018-11-15 16:11:26 +0000

    v0.1.0-alpha.33

--- dist/app/factory.d.ts
+++
@@ -1,8 +0,0 @@
-import { SignStrategyForFactory } from "../structs/factory";
-import AppClient from "./client";
-import EncryptionKey from "./encryption-key";
-import AppParams from "./params";
-declare function buildEncryptionKey(encryptionKey: any, signStrategy: SignStrategyForFactory): Promise<EncryptionKey>;
-declare function buildAppParams(appParams: any, signStrategy: SignStrategyForFactory): Promise<AppParams>;
-declare function buildAppClient(address: string, opts: any): Promise<AppClient>;
-export { buildEncryptionKey, buildAppParams, buildAppClient, };
--- dist/app/params.d.ts
+++
@@ -1,9 +0,0 @@
-import { ReadPermission, Request } from "../structs";
-import EncryptionKey from "./encryption-key";
-export default class AppParams {
-    readonly request: Request;
-    readonly extraReadPermissions: ReadPermission[];
-    readonly version: string;
-    readonly encryptionKey: EncryptionKey;
-    constructor(appParams: any);
-}
